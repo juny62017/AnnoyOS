db09006bc22fcc3cb8a5546d2d94f03f2699dae3
juny62017 <juny62017@gmail.com>
2026-06-04 16:43:53 +0530

    made the basic layout

---
+++ script.js
@@ -0,0 +1,15 @@
+const startBtn =
+document.getElementById(
+"startBtn"
+);
+
+startBtn.addEventListener(
+"click",
+function(){
+
+    alert(
+    "⚠ AnnoyOS is loading...\nProceed at your own risk."
+    );
+
+}
+);
