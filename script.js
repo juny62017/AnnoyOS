7eecfb1b0c385a41860211c2ae93accb0058ecf2
juny62017 <juny62017@gmail.com>
2026-06-05 12:20:44 +0530

    Added moving button system.

--- script.js
+++ script.js
@@ -3,12 +3,92 @@ document.getElementById(
 "startBtn"
 );
 
+const status =
+document.getElementById(
+"status"
+);
+
+const message =
+document.getElementById(
+"message"
+);
+
+let attempts = 0;
+
+startBtn.addEventListener(
+"mouseenter",
+function(){
+
+    attempts++;
+
+    status.innerText =
+    "Attempts to click: " +
+    attempts;
+
+    const maxX =
+    window.innerWidth -
+    startBtn.offsetWidth -
+    50;
+
+    const maxY =
+    window.innerHeight -
+    startBtn.offsetHeight -
+    100;
+
+    const randomX =
+    Math.random() *
+    maxX;
+
+    const randomY =
+    Math.random() *
+    maxY;
+
+    startBtn.style.position =
+    "fixed";
+
+    startBtn.style.left =
+    randomX + "px";
+
+    startBtn.style.top =
+    randomY + "px";
+
+    if(attempts < 5){
+
+        message.innerText =
+        "Too slow 😎";
+
+    }
+
+    else if(attempts < 10){
+
+        message.innerText =
+        "Still trying? 😂";
+
+    }
+
+    else if(attempts < 15){
+
+        message.innerText =
+        "Give up already 🤣";
+
+    }
+
+    else{
+
+        message.innerText =
+        "AnnoyOS is winning 💀";
+
+    }
+
+}
+);
+
 startBtn.addEventListener(
 "click",
 function(){
 
     alert(
-    "⚠ AnnoyOS is loading...\nProceed at your own risk."
+    "Impossible... you clicked it."
     );
 
 }
